668829e1b727719262216e9215919368d54025cf
afzalhossenhasib <ahhasib47@gmail.com>
2022-06-09 06:03:17 +0600

    about added

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
             <div class="hero bg-base-200">
   <div class="hero-content flex-col lg:flex-row-reverse">
     <img src={mypic} class="max-w-sm rounded-lg shadow-2xl" />
-    <div>
+    <div className='ml-7'>
       <h1 class="text-5xl font-bold"><span className='one'>Hi! I Am</span> <br /> <span className='two'>Afzal Hossen Hasib</span></h1>
       <p class="py-6">I'm a Bangladesh based front‑end developer focused on crafting clean and user‑friendly experiences, I am passionate about building excellent website that improves the lives of those around me.</p>
       <button class="btn resume-button">Download Resume</button>
